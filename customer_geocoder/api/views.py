4c73fffe94d700ae984d428d315b5bc0d2ca90da
flych3r <matheus.sampaio011@gmail.com>
2021-05-13 12:20:33 -0300

    added Customer model, serielizer and view

--- customer_geocoder/api/views.py
+++ customer_geocoder/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from customer_geocoder.api.models import Customer
+from customer_geocoder.api.serializers import CustomerSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
