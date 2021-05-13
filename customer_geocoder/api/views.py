d046c891e442ef63669bf066cc9a20e4bc6da25b
flych3r <matheus.sampaio011@gmail.com>
2021-05-13 14:16:45 -0300

    linted files, added logger util

--- customer_geocoder/api/views.py
+++ customer_geocoder/api/views.py
@@ -6,6 +6,8 @@ from customer_geocoder.api.serializers import CustomerSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows Customers to be viewed or edited."""
+
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [permissions.IsAuthenticated]
