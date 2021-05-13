a4b0f29ace911fb01df290187a9d7f0b715ff8b8
flych3r <matheus.sampaio011@gmail.com>
2021-05-13 12:25:09 -0300

    added api routes

--- customer_geocoder/urls.py
+++ customer_geocoder/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
+
+from customer_geocoder.api import views
+
+
+router = routers.DefaultRouter()
+router.register(r'customers', views.CustomerViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
